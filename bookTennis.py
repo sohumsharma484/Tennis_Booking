c86c748dfc141076354197ef08be6873813f26ea
Sohum Sharma <102423861+sohumsharma484@users.noreply.github.com>
2022-10-15 08:54:44 -0500

    Update bookTennis.py

--- bookTennis.py
+++ bookTennis.py
@@ -21,60 +21,6 @@ import time
 from elevate import elevate
 
 elevate()
-#
-# def scheduleTask(bookDate):
-#     elevate()
-#     commands = r'schtasks /query /xml /tn "\test" > "C:\Users\sohum\OneDrive\Documents\Python Scripts\Selenium\tennis\task.xml'
-#     shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c '+commands)
-#
-#     time.sleep(1) # let file load
-#     dateTime = f"{bookDate.split()[0]}T6:59:50"
-#
-#     import xml.etree.ElementTree as ET
-#
-#     with open('test.xml', 'r') as file:
-#         filedata = file.read()
-#
-#     # Replace the target string
-#     filedata = filedata.replace("UTF-16", "UTF-8")
-#
-#     # Write the file out again
-#     with open('test.xml', 'w') as file:
-#         file.write(filedata)
-#
-#     xmlTree = ET.parse("test.xml")
-#     root = xmlTree.getroot()
-#
-#     element = root.find("{http://schemas.microsoft.com/windows/2004/02/mit/task}Triggers").find(
-#         "{http://schemas.microsoft.com/windows/2004/02/mit/task}TimeTrigger").find(
-#         "{http://schemas.microsoft.com/windows/2004/02/mit/task}StartBoundary")
-#     element.text = dateTime
-#
-#     element = root.find("{http://schemas.microsoft.com/windows/2004/02/mit/task}Actions").find(
-#         "{http://schemas.microsoft.com/windows/2004/02/mit/task}Exec").find(
-#         "{http://schemas.microsoft.com/windows/2004/02/mit/task}Arguments")
-#     element.text = bookDate
-#
-#     xmlTree.write("test.xml", encoding='UTF-8', xml_declaration=True)
-#
-#     with open('test.xml', 'r') as file:
-#         filedata = file.read()
-#
-#     # Replace the target string
-#     filedata = filedata.replace("UTF-8", "UTF-16")
-#
-#     # Write the file out again
-#     with open('test.xml', 'w') as file:
-#         file.write(filedata)
-#
-#     commands = r'schtasks /create /xml "C:\Users\sohum\OneDrive\Documents\Python Scripts\Selenium\tennis\task.xml" /tn "\test2"'
-#     shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c '+commands)
-
-
-# PATH = "C:\Program Fi les (x86)\chromedriver.exe"
-# driver = webdriver.Chrome(PATH)
-#
-# driver.get("https://canyoncreekhoa.tennisbookings.com/LoginX.aspx")
 
 root = tk.Tk()
 times = {"5 AM": 1, "5:30 AM": 2, "6 AM": 3, "6:30 AM": 4, "7 AM": 5, "7:30 AM": 6, "8 AM": 7, "8:30 AM": 8, "9 AM": 9, "9:30 AM": 10, "10 AM": 11, "10:30 AM": 12, "11 AM": 13, "11:30 AM": 14, "12 PM": 15, "12:30 PM": 16, "1 PM": 17, "1:30 PM": 18, "2 PM": 19, "2:30 PM": 20, "3 PM": 21, "3:30 PM": 22, "4 PM": 23, "4:30 PM": 24, "5 PM": 25, "5:30 PM": 26, "6 PM": 27, "6:30 PM": 28, "7 PM": 29, "7:30 PM": 30, "8 PM": 31, "8:30 PM": 32, "9 PM": 33, "9:30 PM": 34, "10 PM": 35}
